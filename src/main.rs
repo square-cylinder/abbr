e78b493f9938630ab856d4a7c031da1f20da68f3
Hugo Söderbergh <hugo.soderbergh007@gmail.com>
2023-04-09 23:13:57 +0200

    Add argument parsing and some directory exploring

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+use std::{path::{Path, PathBuf}, fs};
+
+use clap::{Subcommand, Parser};
+
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    /// Resolve an abbreviation
+    Get {
+        #[clap(forbid_empty_values=true)]
+        /// The abbreviation you want to look up
+        abbr: String,
+    },
+    /// Add a new abbreviation
+    Put {
+        #[clap(forbid_empty_values=true)]
+        /// The abbreviation you want to add
+        abbr: String,
+        /// What it means
+        full: String,
+    },
+}
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(subcommand)]
+    cmd: SubCommand,
+}
+
+fn get_documents() -> Result<PathBuf, String> {
+    if let Some(user_dirs) = directories::UserDirs::new() {
+        if let Some(documents) = user_dirs.document_dir() {
+            return Ok(documents.to_path_buf());
+        }
+    }
+    Err("Could not retrieve users document folder".to_owned())
+}
+
+fn get_program_directory() -> Result<PathBuf, String> {
+    let mut path = get_documents()?;
+    path.push("abbr");
+    match fs::create_dir_all(&path) {
+        Ok(()) => Ok(path),
+        Err(e) => Err(e.to_string())
+    }
+}
+
+fn get_storage() -> Result<PathBuf, String> {
+    let mut path = get_program_directory()?;
+    path.push("storage.txt");
+    Ok(path)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    match args.cmd {
+        SubCommand::Get { abbr } => {
+            todo!();
+        },
+        SubCommand::Put { abbr, full } => {
+            todo!();
+        }
+    }
 }
